1f644e5cca05c1e5382f485604c031ee649b79a1
Evgeny Kovalev <eikovalev@edu.hse.ru>
2018-10-18 10:59:10 +0300

    changed lgbm to catboost

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import argparse
-import lightgbm as lgb
 import os
 import pickle
+import shutil
 import time
+from catboost import CatBoostRegressor, CatBoostClassifier
 from fp import load_data
+from sklearn.model_selection import train_test_split
 
 # use this to stop the algorithm before time limit exceeds
 TIME_LIMIT = int(os.environ.get('TIME_LIMIT', 5*60))
@@ -17,40 +19,66 @@ if __name__ == '__main__':
     parser.add_argument('--mode', choices=['classification', 'regression'], required=True)
     args = parser.parse_args()
 
+    if not os.path.exists(args.model_dir):
+        os.mkdir(args.model_dir)
+    
     start_time = time.time()
 
     X_train, y_train, model_config, _ = load_data(args.train_csv)
 
     model_config['mode'] = args.mode
-
-    params =  {
-	'task': 'train',
-	'boosting_type': 'gbdt',
-	'objective': 'regression' if args.mode == 'regression' else 'binary',
-	'metric': 'rmse',
-	"learning_rate": 0.01,
-	"num_leaves": 200,
-	"feature_fraction": 0.70,
-	"bagging_fraction": 0.70,
-	'bagging_freq': 4,
-	"max_depth": -1,
-        "verbosity" : -1,
-	"reg_alpha": 0.3,
-	"reg_lambda": 0.1,
-	#"min_split_gain":0.2,
-	"min_child_weight":10,
-	'zero_as_missing':True,
-        'num_threads': 4,
-    }
-
-    params['seed'] = 1
-    model = lgb.train(params, lgb.Dataset(X_train, label=y_train), 600)
-
-    model_config['model'] = model
-    model_config['params'] = params
-
+    
     model_config_filename = os.path.join(args.model_dir, 'model_config.pkl')
     with open(model_config_filename, 'wb') as fout:
         pickle.dump(model_config, fout, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    # eval dataset during the training
+    size = min(X_train.shape[0] // 10, 1000)
+    _, X_eval, _, y_eval = train_test_split(X_train, y_train, test_size=size)
+
+    train_dir = 'catboost_info/'
+    if os.path.exists(train_dir):
+        shutil.rmtree(train_dir)
+
+    model_params = {"iterations": 0,
+                    "one_hot_max_size": 10,
+                    "random_seed": 13,
+                    "nan_mode": 'Min',
+                    "depth": 6,
+                    "used_ram_limit":'512mb',
+                    "loss_function": 'Logloss',
+                    #"save_snapshot": True,
+                    # "custom_metric": 'AUC:hints=skip_train~false',
+                    # "metric_period": 20,
+                    "train_dir": train_dir,
+                    }
+    
+     # train the model until time allows
+    total_iter = 0 # total number of iterations to train
+    iter_time = 0 # last time for one iteration
+    # if TIME_LIMIT > 300:
+        #-240
+    while iter_time < TIME_LIMIT - (time.time() - start_time) - 60:
+        start_iter = time.time()
+        total_iter = total_iter + 100
+        model_params["iterations"] = total_iter
+        if args.mode == 'regression':
+            model_params["loss_function"] = "RMSE"
+            model = CatBoostRegressor(**model_params)
+        else:
+            pos_weight = X_train[y_train < 0.5].size / X_train[y_train > 0.5].size
+            model_params["scale_pos_weight"] = pos_weight
+            model = CatBoostClassifier(**model_params)
+
+        model.fit(X_train, y_train,
+                  logging_level='Silent',
+                  use_best_model=True,
+                  # early_stopping_rounds=10,
+                  eval_set=[(X_eval, y_eval)],
+                  )
+        model.save_model(os.path.join(args.model_dir, 'model.catboost'))
+        iter_time = time.time() - start_iter
+        print('Time per iteration: {}'.format(iter_time))
+
 
     print('Train time: {}'.format(time.time() - start_time))
